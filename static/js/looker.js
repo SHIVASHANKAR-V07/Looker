2323e781b0fac9c0f6887a192587e2f903923ace
SHIVASHANKAR-V07 <vshivashankar07@gmail.com>
2025-07-14 22:35:16 +0530

    Update looker.js

--- static/js/looker.js
+++ static/js/looker.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('home').style.display = 'block';
+  }, 1000);
+ });
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -7,4 +14,9 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
+}
+
+function toggleProfile() {
+  const menu = document.getElementById('profile-menu');
+  menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
 }
